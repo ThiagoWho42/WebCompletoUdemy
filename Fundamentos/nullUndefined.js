e8cccf88097114c9e7666b3d9e477c78d85f8521
ThiagoWho42 <thiago.einteinnnn@gmail.com>
2018-09-19 14:53:58 -0400

    funcao

--- Fundamentos/nullUndefined.js
+++ Fundamentos/nullUndefined.js
@@ -1,1 +1,15 @@
 let valor // não inicializada
+console.log(valor)
+
+valor = null //ausencia de valor
+console.log(valor)
+// console.log(valor.toString()) // Erro!
+
+const produto ={}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined // evite atribuir undefined
